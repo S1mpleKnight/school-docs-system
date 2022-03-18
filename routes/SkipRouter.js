aba79b02a9dffe44287f92bd3c6ce15c648e7152
S1mpleKnight <become.adult77@gmail.com>
2022-03-18 03:31:06 +0300

    delete: skip router

--- routes/SkipRouter.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const skipController = require('../controller/SkipController')
-
-router.post('/', skipController.create)
-router.get('/', skipController.findAll)
-
-module.exports = router
