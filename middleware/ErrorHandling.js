46aaf262bd2b0ae3e51582a501e27b068bb9f920
S1mpleKnight <become.adult77@gmail.com>
2022-03-30 02:15:34 +0300

    fix: unpack message from errors in ErrorHandling

--- middleware/ErrorHandling.js
+++ middleware/ErrorHandling.js
@@ -1,8 +1,8 @@
 const ApiError = require('../error/ApiError')
 
-module.exports = function (err, req, res, next) {
+module.exports = function (err, req, res) {
     if (err instanceof ApiError) {
         return res.status(err.status).json({message: err.message})
     }
-    return res.status(500).json({message: 'Unexpected error'})
+    return res.status(500).json({message: `Unexpected error: ${err}`})
 }
