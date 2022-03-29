4b95bceea4f6fa7a88ed20afada0a15101321a99
S1mpleKnight <become.adult77@gmail.com>
2022-03-30 02:12:26 +0300

    new: add VoucherController

---
+++ controller/VoucherController.js
@@ -0,0 +1,47 @@
+const {Voucher} = require('../models/models')
+const apiError = require('../error/ApiError')
+
+class VoucherController {
+    async findAll(req, res, next) {
+        try {
+            const rawVouchers = await Voucher.findAll()
+            const vouchers = JSON.parse(JSON.stringify(rawVouchers))
+            let responseBody = [];
+            for (const voucher of vouchers) {
+                const {documentUrl, uploadDate, id} = voucher
+                responseBody.push({id, documentUrl, uploadDate})
+            }
+            return res.json(responseBody)
+        } catch (e) {
+            console.log(`Error in the VoucherController findAll method ${e}`)
+            next(apiError.badRequest(e.message))
+        }
+    }
+
+    async uploadVoucher(req, res, next) {
+        try {
+            const documentUrl = req.file.path
+            const uploadDate = Date.now()
+            const voucher = await Voucher.create({documentUrl, uploadDate})
+            return res.json(voucher)
+        } catch (e) {
+            console.log(`Error in the VoucherController upload method ${e}`)
+            next(apiError.badRequest(e.message))
+        }
+    }
+
+    async findById(req, res, next) {
+        try {
+            const voucher = await Voucher.findOne({where: {"id": req.params.id}})
+            if (!voucher) {
+                return next(apiError.notFound(`voucher with id: ${req.params.id} do not exist`))
+            }
+            return res.json(voucher)
+        } catch (e) {
+            console.log(`Error in the VoucherController findById method ${e}`)
+            next(apiError.badRequest(e.message))
+        }
+    }
+}
+
+module.exports = new VoucherController()
