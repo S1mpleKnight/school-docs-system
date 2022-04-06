b4b02baeeb52113b8de62bfe746259c6bdd9565b
S1mpleKnight <become.adult77@gmail.com>
2022-04-06 14:41:59 +0300

    fix: add cors to the server

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ const PORT = config.get('port') || 5000
 
 const app = express()
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+
+    app.options('*', (req, res) => {
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.send();
+    });
+});
+
 app.use(express.json())
 app.use('/api', router)
 app.use(errorHandler)
