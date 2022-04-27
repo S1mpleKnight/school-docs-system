97cfe8f2690b3c810446a1275468df7cf16ec6bb
Nibbler <saymooon4358@gmail.com>
2022-04-27 17:58:09 +0300

    marks almost done

--- service/MarkService.js
+++ service/MarkService.js
@@ -1,12 +1,19 @@
 const {Mark, Subject, User, MarkRole, Term} = require('../models/models')
 const apiError = require('../error/ApiError')
-const {validationResult} = require("express-validator");
+const {validationResult} = require("express-validator")
+const {Op} = require('sequelize');
+
 const parsingErrors = require("../error/ErrorParser");
 
 class MarkService {
     async findAll(req, res, next) {
         try {
-            const marks = await Mark.findAll()
+            const marks = await Mark.findAll({
+                where: {
+                    "markRoleId": 1
+                }
+            })
+
             console.log('\x1b[32m%s\x1b[0m', `Marks send: ${marks.length} date: ${new Date(Date.now()).toUTCString()}}`)
             return res.json(marks)
         } catch (e) {
@@ -22,28 +29,27 @@ class MarkService {
                 let errorMessages = parsingErrors(errors);
                 return next(apiError.badRequest([...errorMessages]))
             }
-            const {studentId, teacherId, value, markRoleId, subjectId, termId} = req.body
-            let {date} = req.body
-            if (!await User.findOne({where: {"id": studentId}})) {
-                return next(apiError.notFound(`Student with id: ${studentId} does not exist`))
+            const teacherId = req.userId
+            let {student, subject, date, value, termId} = req.body
+            if (!await User.findOne({where: {"id": student}})) {
+                return next(apiError.notFound(`Student with id: ${student} does not exist`))
             }
-            if (!await User.findOne({where: {"id": teacherId}})) {
-                return next(apiError.notFound(`Teacher with id: ${teacherId} does not exist`))
+            if (!await Subject.findOne({where: {"id": subject}})) {
+                return next(apiError.notFound(`Subject with id: ${subject} does not exist`))
             }
-            if (!await MarkRole.findOne({where: {"id": markRoleId}})) {
-                return next(apiError.notFound(`MarkRole with id: ${markRoleId} does not exist`))
+            if (termId) {
+                const term = await Term.findOne({where: {"id": termId}})
+                if (!term) {
+                    return next(apiError.notFound(`Term with id: ${termId} does not exist`))
+                }
             }
-            if (!await Subject.findOne({where: {"id": subjectId}})) {
-                return next(apiError.notFound(`Subject with id: ${subjectId} does not exist`))
-            }
-            if (!await Term.findOne({where: {"id": termId}})) {
-                return next(apiError.notFound(`Term with id: ${termId} does not exist`))
-            }
-            const student = studentId
+            const markRole = await MarkRole.findOne({where: {
+                "name": 'LESSON'
+                }})
+            const markRoleId = markRole.id
             const teacher = teacherId
-            const subject = subjectId
             date = new Date(date)
-            const mark = await Mark.create({student, teacher, subject, termId, value, markRoleId, date})
+            const mark = await Mark.create({date, subject, student, teacher,  value, markRoleId, termId})
             return res.json(mark)
         } catch (e) {
             console.log('\x1b[31m%s\x1b[0m', `Error in the MarkService creation method ${e}`)
@@ -53,19 +59,19 @@ class MarkService {
 
     async delete(req, res, next) {
         try {
-            const {subjectId, studentId, date} = req.body
-            if (!await Subject.findOne({where: {"id": subjectId}})) {
+            const {subject, student, date} = req.body
+            if (!await Subject.findOne({where: {"id": subject}})) {
                 return next(apiError.notFound(`Subject with id: ${id} does not exist`))
             }
-            const student = await User.findOne({where: {"id": studentId}})
-            if (!student || student.roleId !== 3) {
+            const studentInstance = await User.findOne({where: {"id": student}})
+            if (!studentInstance || studentInstance.roleId !== 3) {
                 return next(apiError.notFound(`Student with id: ${id} does not exist`))
             }
             const mark = await Mark.findOne(
                 {
                     where: {
-                        "subject": subjectId,
-                        "student": studentId,
+                        "subject": subject,
+                        "student": student,
                         "date": date
                     }
                 })
@@ -74,8 +80,8 @@ class MarkService {
             }
             await Mark.destroy({
                 where: {
-                    "subject": subjectId,
-                    "student": studentId,
+                    "subject": subject,
+                    "student": student,
                     "date": date
                 }
             })
@@ -130,12 +136,64 @@ class MarkService {
         }
     }
 
+    async findTeachersMarksCurrentTerm(req, res, next) {
+        try {
+            const teacherId = req.userId
+            const date = new Date()
+            const term = await Term.findOne({
+                where: {
+                    [Op.and]: [
+                        {
+                            startDate: {
+                                [Op.lte]: date
+                            }
+                        },
+                        {
+                            endDate: {
+                                [Op.gte]: date
+                            }
+                        }
+                    ]
+                }
+            })
+            const markList = await Mark.findAll({
+                where: {
+                    teacher: teacherId,
+                    markRoleId: 1,
+                    date: {
+                        [Op.gte]: term.startDate
+                    }
+                }
+            })
+
+            let fullInfo = []
+            let markInfo;
+            for (markInfo of markList) {
+                let fullMark = {}
+                const subject = await Subject.findOne({
+                    where: {
+                        id: markInfo.subject
+                    }
+                })
+                fullMark.student = markInfo.student
+                fullMark.subject = subject.name
+                fullMark.value = markInfo.value
+                fullMark.date = markInfo.date
+                fullInfo.push(fullMark)
+            }
+            console.log('\x1b[32m%s\x1b[0m', `Marks sent: ${fullInfo.length} date: ${new Date(Date.now()).toUTCString()}}`)
+            return res.json(fullInfo)
+        } catch (e) {
+            console.log('\x1b[31m%s\x1b[0m', `Error in the MarkController findStudentTermMarks method ${e}`)
+            next(apiError.badRequest(e.message))
+        }
+    }
+
     async findStudentTermMarks(req, res, next) {
         try {
             const markList = await Mark.findAll({
                 where: {
-                    termId: req.params.term,
-                    student: req.params.id
+                    teacher: req.params.id
                 }
             })
             let fullInfo = []
